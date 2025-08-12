56b22c9256291465f726103fee5a7ebd560ae55b
Anita Dhlamini <anilicula@gmail.com>
2025-08-13 00:59:26 +0200

    Add library_system.py script

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,5 +1,3 @@
-# library_system.py
-
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -8,16 +6,13 @@ class Book:
     def get_details(self):
         return f"Book: {self.title} by {self.author}"
 
-
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size = file_size  # in KB
+        self.file_size = file_size
 
     def get_details(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
-
-
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
@@ -25,8 +20,6 @@ class PrintBook(Book):
 
     def get_details(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-
-
 class Library:
     def __init__(self):
         self.books = []
@@ -35,9 +28,8 @@ class Library:
         if isinstance(book, Book):
             self.books.append(book)
         else:
-            raise TypeError("Only Book instances can be added to the library.")
+            raise TypeError("Only instances of Book or its subclasses are allowed.")
 
     def list_books(self):
         for book in self.books:
             print(book.get_details())
-
